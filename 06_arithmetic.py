418680d069f5bf654aaccdff85cbdfcdd88eee71
HARIHARAN B P <bphariharan1301@gmail.com>
2022-06-13 13:25:29 +0530

    Day-1: Pushed

---
+++ 06_arithmetic.py
@@ -0,0 +1,18 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+
+print(a+b)
+print(a-b)
+print(a % b)
+print(a*b)
+print(a/b)
+print(a**b)
+
+'''a = 5
+b = 2
+# Use BODMAS here so, bracket -> power ->  division -> addition -> substraction
+print((a+b) - b**a + a/b)
+
+print((a-b + a*b - a**b/a + a % b))
+'''
---
+++ 12_odd_rev_cube.py
@@ -0,0 +1,23 @@
+'''
+take int as input
+if it is odd print reverse + 23
+if even print it's cube root
+'''
+
+
+def rev_num(n):
+    dig = 0
+    m = n
+    while(m != 0):
+        r = m % 10
+        dig = dig*10 + r
+        m = m//10
+    return(dig+23)
+
+
+n = int(input("Enter the Number: "))
+
+if n % 2 == 0:
+    print(n**(1/3))
+else:
+    print(rev_num)
